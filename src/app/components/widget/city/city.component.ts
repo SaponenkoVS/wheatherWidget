638c98aed5902e76ac256d67b0e3e9766754c2dd
vlad <saponenkovladislav2001@mail.ru>
2023-07-22 15:16:06 +0200

    minimal style changes

--- src/app/components/widget/city/city.component.ts
+++ src/app/components/widget/city/city.component.ts
@@ -13,11 +13,11 @@ import {animate, style, transition, trigger} from "@angular/animations";
   animations: [
     trigger('slideInOut', [
       transition(':enter', [
-        style({transform: 'translateX(-100%)', opacity: 0}),
+        style({transform: 'translateY(-100%)', opacity: 0}),
         animate('300ms', style({transform: 'translateX(0)', opacity: 1})),
       ]),
       transition(':leave', [
-        animate('300ms', style({transform: 'translateX(100%)', opacity: 0})),
+        animate('300ms', style({transform: 'translateY(100%)', opacity: 0})),
       ]),
     ]),
   ],
